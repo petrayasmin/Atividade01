491ca7389919cfc89fb955ebd736c0a572cd109b
petrayasmin <petra_mineiro@outlook.com>
2024-11-05 21:33:36 -0300

    alterações finaisssss

--- Q1/calcularIdadeCJS.js
+++ Q1/calcularIdadeCJS.js
@@ -2,6 +2,6 @@ const prompt = require("prompt-sync")();
 const calcularIdade = require('./calcularIdade.js');
 
 
-const anoNasc = Number(prompt(`Digite ano de nascimento: `));
+const anoNasc = parseInt(prompt("Digite ano de nascimento: "));
 
 console.log("Sua idade: ", calcularIdade.calculateAge(anoNasc));
